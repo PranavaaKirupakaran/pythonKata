586b6610af01c1ffc440d378af50ae479a9f01b8
PranavaaKirupakaran <pranavaa.kirupakaran@gmail.com>
2020-07-25 09:10:56 -0400

    barely more efficient

--- squareDigit.py
+++ squareDigit.py
@@ -2,8 +2,7 @@ def square_digit(num):
     numString = str(num)
     numSquareString = ""
     for n in numString:
-        numSquareString = numSquareString + str(int(n)*int(n))
-
+        numSquareString += str(int(n)*int(n))
     return int(numSquareString)
 
 print(square_digit(9119))
