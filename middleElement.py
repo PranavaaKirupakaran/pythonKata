0adf4e4aa035c75909d1c3e0db986e77c9e65035
PranavaaKirupakaran <pranavaa.kirupakaran@gmail.com>
2020-07-25 08:51:58 -0400

    more efficient using shallow copy of array

--- middleElement.py
+++ middleElement.py
@@ -3,7 +3,6 @@ input2 = input("Element 2: \n")
 input3 = input("Element 3: \n")
 
 input_array = [input1, input2, input3]
-inputCopy = [input1, input2, input3]
+inputCopy = input_array.copy()
 inputCopy.sort()
-print(input_array)
 print(input_array.index(inputCopy[1]))
